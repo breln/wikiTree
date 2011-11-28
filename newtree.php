57be3172f1b03426446203cbd6821fcc501a6d1d
Max Kotlyarsky <brain1533@gmail.com>
2011-11-29 00:11:04 +0400

    Replaced echoing results into browser with fwrite

--- newtree.php
+++ newtree.php
@@ -1,52 +1,46 @@
 <?php
-//to be commented later
-echo "Hi";
 ini_set("display_errors", 1);
 ini_set("error_reporting", E_ALL ^ E_WARNING);
-
 set_time_limit(0);
+
 $time_start = microtime(true);
-function getDirectoryListing($path, $cat){
-	// fetching the page to local storage using cURL to avoid problems with Cyrillic URLs
-	//echo "Loading - ".$cat."<br>";
+function getDirectoryListing($result, $path, $cat){
 	$path .= " > ".$cat;
 	$cat = str_replace(" ", "_", $cat);
 	$ch = curl_init();
-	$fp = fopen("testtt.html", "w");
+	$buffer = fopen("buffer.html", "w");
 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2");
 	curl_setopt($ch, CURLOPT_URL, "http://ru.wikipedia.org/wiki/Категория:".$cat);
-	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_FILE, $buffer);
 	curl_exec($ch);
 	curl_close ($ch);
-	fclose($fp);
+	fclose($buffer);
 
 	// working with the pre-fetched page using DOMDocument
+	// due to a bug (?) w/ fetching Cyrillic URLs via DOMDocument->loadHTMLFile
 	$html = new DOMDocument();
-	$html->loadHTMLFile("testtt.html");
+	$html->loadHTMLFile("buffer.html");
 	
 	$subCats = $html->getElementById("mw-subcategories");
 
 	if($subCats){
-		$subCatLinks = $subCats->getElementsByTagName("a");
-		foreach ($subCatLinks as $s){
-			//echo "Length: ".$subCatLinks->length;
-			//echo $path." > <b>".$s->textContent."</b><br>";
-			getDirectoryListing($path, $s->textContent);
+		foreach ($subCats->getElementsByTagName("a") as $s){
+			getDirectoryListing($result, $path, $s->textContent);
 		}
 	}
-	//$articles = $html->getElementById("mw-pages")->getElementsByTagName("a");
 	$mwpages = $html->getElementById("mw-pages");
 	if($mwpages) {
 		foreach ($mwpages->getElementsByTagName("a") as $a){
-			echo $path." > <b>".$a->textContent."</b><br>";
+			fwrite($result, "$path > $a->textContent".PHP_EOL);
 		}
 	}
-	//else echo "NOOOOO!";
 }
-getDirectoryListing(null, "Дискретная математика");
+$result = fopen("result.txt", "w");
+getDirectoryListing($result, null, "Дискретная математика");
+fclose($result);
 
 $time_end = microtime(true);
 $time = $time_end - $time_start;
 
-echo "<br><br>Finished in $time seconds";
+echo "<br>Finished in $time seconds";
 ?>
